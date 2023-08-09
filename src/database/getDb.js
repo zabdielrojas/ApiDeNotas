41ea060b4773d6eb16bf4e03a881aa7c14cae72d
Zabdiel Rojas <zabdielrojas5@gmail.com>
2023-08-09 18:42:53 +0200

    feat getDb añade la funcion de conectar a la base de datos

---
+++ src/database/getDb.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql2/promise");
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
+let pool;
+
+const getDb = async () => {
+  try {
+    if (!pool) {
+      const dbConnection = await mysql.createConnection({
+        connectionLimit: 10,
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE,
+        timezone: "Z",
+      });
+
+    }
+    
+    // Retornamos una conexión libre.
+    return await pool.getConnection();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Exportamos la función.
+module.exports = getDb;
