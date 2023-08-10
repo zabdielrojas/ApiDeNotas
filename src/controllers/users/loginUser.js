808f44ee23f80d00f3274892cf2d7ca879239316
arianarumbos <arianarumbos@gamil.com>
2023-08-10 16:35:50 +0200

    feat loginUser añade el controlador de login de usuarios

---
+++ src/controllers/users/loginUser.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const {generateError} = require("../../helpers") 
+const selectUserByEmailQuery = require("../../database/queries/users/selectUserByEmailQuery")
+
+const loginUser = async(req,res,next) => {
+
+    try {
+        const {email,password}=req.body
+
+        if(!email|| !password){
+            throw generateError("faltan campos", 404)
+        }
+
+        const user =await selectUserByEmailQuery(email)
+
+        const validPassword =await bcrypt.compare(password,user.password)
+
+        if(!validPassword){
+            throw generateError("contraseña incorrecta",401)
+        }
+
+        const tokenInfo={
+            id:user.id 
+        }
+
+        const token = jwt.sign(tokenInfo,process.env.SECRET,{expiresIn:"30d"})
+        res.send({status:"ok",data:{token}})
+    } catch (error) {
+        next(error)
+    }
+}
+ 
+module.exports =loginUser
