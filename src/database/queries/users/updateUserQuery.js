7a5fc02f9bb4b1b4711bfba87d63eaaf1ec04347
arianarumbos <arianarumbos@gamil.com>
2023-08-10 21:54:46 +0200

    feat updateUserQuery añade la Query de editar usuario

---
+++ src/database/queries/users/updateUserQuery.js
@@ -0,0 +1,22 @@
+const getDb = require("../../getDb");
+
+const updateUserQuery = async (updatedUser) => {
+  const { id, username, email } = updatedUser;
+
+  //Declaramos la conexión para poder acceder en los bloques de try catch y finally.
+  let connection;
+  try {
+    // Abrimos una pool de conexiones con la base de datos.
+    connection = await getDb;
+
+    //actualizamos la informacion del usuario.
+    await connection.query(
+      `UPDATE users SET email=?, username=? WHERE id = ?`,
+      [email, username, id]
+    );
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = updateUserQuery;
