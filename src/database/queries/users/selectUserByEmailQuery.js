66f9d2577577e0b3f97988427fb6920998b67d69
Zabdiel Rojas <zabdielrojas5@gmail.com>
2023-08-10 14:08:36 +0200

    Feat selectUserByEmailQuery añade la query para seleccionar un usuario por su email

---
+++ src/database/queries/users/selectUserByEmailQuery.js
@@ -0,0 +1,29 @@
+const getDb = require("../../getDb");
+
+const {generateError}= require("../../../helpers")
+
+const selectUserByEmailQuery = async (email)=>{
+ //Declaramos la conexión para poder acceder en los bloques de try catch y finally.
+ let connection;
+try {
+    // Abrimos una pool de conexiones con la base de datos.
+    connection =  getDb
+
+    //Seleccionamos al usuario con el email de la petición.
+    const [users] = await connection.query(`SELECT * FROM users WHERE email = ?`,[email]);
+
+ //Lanzamos un error si el email es incorrecto
+ if (users.length < 1) {
+    throw generateError("Usuario no encontrado", 404);
+  }
+  //Devolvemos la información del usuario.
+  return users[0]
+
+}finally{
+    if(connection) connection.release();
+}
+
+
+}
+
+module.exports = selectUserByEmailQuery;
