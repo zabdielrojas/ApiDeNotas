2d09d3c4fbf804b6f6ad3e8eb7111c6ff5f826e1
Zabdiel Rojas <zabdielrojas5@gmail.com>
2023-08-10 13:48:54 +0200

    Feat helpers añade fichero y funcion manejadora de errores

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+
+/**
+ * ####################
+ * ## GENERATE ERROR ##
+ * ####################
+ */
+//Función que nos permite añadir un status code a los errores.
+
+const generateError = (msg, status) => {
+    const error = new Error(msg);
+    error.statusCode = status;
+    return error;
+  };
